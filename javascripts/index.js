bf50546bef241569343c90b1cb55c4d8d282e66c
AngelineChang <s6079601@gmail.com>
2019-05-03 02:29:57 +0800

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,16 +9,106 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+var storage = firebase.storage();
+var storageRef = storage.ref();
+var imagesRef = storageRef.child('images');// Points to 'images'
 
-//change h1 text
-var bigOne = document.getElementById('bigOne');
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', function(snapshot){
-  bigOne,innerText = snapshot.val()
-});
-//dbRef.on('value', snap => bigOne.innerText = snap.val());
-console.log('hi');
+
+
+
+// var imgRef = [];
+// var imgSerial = [];
+// var imgID = [];
+// var imgSrc = [];
+// var imgURL = [];
+// for (var i = 0; i < 3; i++) {
+//   imgSerial[i] = "images/" + i + ".png";
+//   imgRef[i] = storageRef.child(imgSerial[i]);
+//   imgURL[i] = imgRef[i].getDownloadURL()
+//   imgID[i] = document.getElementById(i);
+//   imgSrc[i] = imgID[i].src;
+// }
+
+
+  var imgRef0 = storageRef.child('images/0.png');
+  imgRef0.getDownloadURL().then(function(url) {
+    var img = document.getElementById('0');
+    img.src = url;
+  });
+
+  var imgRef1 = storageRef.child('images/1.png');
+  imgRef1.getDownloadURL().then(function(url) {
+    var img = document.getElementById('1');
+    img.src = url;
+  });
+
+  var imgRef2 = storageRef.child('images/2.png');
+  imgRef2.getDownloadURL().then(function(url) {
+    var img = document.getElementById('2');
+    img.src = url;
+  });
+
+  var imgRef3 = storageRef.child('images/3.png');
+  imgRef3.getDownloadURL().then(function(url) {
+    var img = document.getElementById('3');
+    img.src = url;
+  });
+
+  var imgRef4 = storageRef.child('images/4.png');
+  imgRef4.getDownloadURL().then(function(url) {
+    var img = document.getElementById('4');
+    img.src = url;
+  });
+
+  var imgRef5 = storageRef.child('images/5.png');
+  imgRef5.getDownloadURL().then(function(url) {
+    var img = document.getElementById('5');
+    img.src = url;
+  });
+
+  var imgRef6 = storageRef.child('images/6.png');
+  imgRef6.getDownloadURL().then(function(url) {
+    var img = document.getElementById('6');
+    img.src = url;
+  });
+
+
+
+
+// var imgRef= storageRef.child('images/0.png');
+// imgRef.getDownloadURL().then(function(url) {
+//   // `url` is the download URL for 'images/stars.jpg'
+//   // Or inserted into an <img> element:
+//   var img = document.getElementById('0');
+//   img.src = url;
+// });
+
+
+// Points to 'images/hp-hangee-intro.png'
+// Note that you can use variables to create child values
+var fileName = '01.png';
+var spaceRef = imagesRef.child(fileName);
+
+// File path is 'images/hp-hangee-intro.png'
+var path = spaceRef.fullPath
+// File name is 'hp-hangee-intro.png'
+var name = spaceRef.name
+// Points to 'images'
+var imagesRef = spaceRef.parent;
+
+var gsReference = storage.refFromURL('gs://hangee-test.appspot.com/images/01.png');
+
+
+
+
+// //change h1 text
+// var bigOne = document.getElementById('bigOne');
+// var dbRef = firebase.database().ref().child('text');
+// dbRef.on('value', function(snapshot){
+//   bigOne,innerText = snapshot.val()
+// });
+// //dbRef.on('value', snap => bigOne.innerText = snap.val());
+// console.log('hi');
 
 
 // <script>
