2de86446c90a795873158fabb64ba7a46febe549
AngelineChang <s6079601@gmail.com>
2019-05-26 01:36:15 +0800

    Update imgControl.js

--- aurora/imgControl.js
+++ aurora/imgControl.js
@@ -1,7 +1,9 @@
 console.log("io here ok");
 
-const socket = io.connect('https://192.168.0.103:443');
-
+const socket = require('socket.io-client')('https://192.168.0.103:443', {
+    transports: ['websocket'],
+    rejectUnauthorized: false
+});
 
 socket.on('screen', function (message) {
     $( "#aurora-pics" ).append( '<img src=" ' + message.url + ' "class="snapshot">' );
