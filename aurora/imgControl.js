8914af50459490f56bd950fcf78aefe810626911
AngelineChang <s6079601@gmail.com>
2019-05-26 02:11:50 +0800

    Update imgControl.js

--- aurora/imgControl.js
+++ aurora/imgControl.js
@@ -1,7 +1,8 @@
 console.log("io here ok");
 
-const socket = io.connect('https://192.168.0.103:443', {
+const socket = io.connect('//192.168.0.103:443', {
 //     transports: ['websocket'],
+    secure: true,
     rejectUnauthorized: false
 });
 
